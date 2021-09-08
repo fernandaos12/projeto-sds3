aa20cc10fa052037968e2464b74e36e9e9d1612a
fernandaos12 <fernandaos12@gmail.com>
2021-09-07 21:16:32 -0300

    Components Statics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="primary">Olá mundo!</h1>
-    </div>
+    <>
+    <NavBar />
+      <div className="Container">
+        <h1 className="txt-primary">Olá mundo!</h1>        
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/fernandaos12" target="_blank" rel="noreferrer">Fernanda Oliveira</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Desenvolvido por: <a href="https://github.com/fernandaos12" target="_blank" rel="noreferrer">@fernandaos12</a></small></p>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;

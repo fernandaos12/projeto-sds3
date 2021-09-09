f4ebac34b5afe7f37302f14e8a3719f1a774598b
fernandaos12 <fernandaos12@gmail.com>
2021-09-09 07:56:59 -0300

    Security Config Cors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,14 +9,14 @@ function App() {
     <>
     <NavBar />
       <div className="Container">
-        <h1 className="txt-primary py-3">Dasboard Vendas</h1>
+        <h1 className="text-primary py-3">Dasboard Vendas</h1>
         <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
             <BarChart />
           </div>
           <div className="col-sm-6">
-            <h5>Total Vendas</h5>
+            <h5 className="text-center text-secondary">Total Vendas</h5>
             <DonutChart />
           </div>
         </div>    

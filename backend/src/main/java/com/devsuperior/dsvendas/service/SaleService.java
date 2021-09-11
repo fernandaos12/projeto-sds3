dcfe1f092ee6581663e63f6f253b17db5ec9d98c
fernandaos12 <fernandaos12@gmail.com>
2021-09-10 22:50:15 -0300

    pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dsvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	//declarar dependencia com o seller repository
+	@Autowired //automatizar a dependencia 
+	private SaleRepository repository;
+	
+	//guardando a consulta na memoria temporaria para diminuir o trafego no bd, somente para bd pequeno
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){  //paginado
+		
+		sellerRepository.findAll();//trazendo pra memoria e armazendo em cache
+		Page<Sale> result = repository.findAll(pageable); //acoes do bd
+		return result.map(x -> new SaleDTO(x));
+	}
+}
+//registrar como componente 
+
